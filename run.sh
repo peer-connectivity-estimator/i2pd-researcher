426ad5ae2a2f7d1efc6516f3b775e2d8ed6bc919
simewu <swuthier@uccs.edu>
2023-07-31 21:54:28 -0600

    Add ability to control log file path

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
-now=$(date +'%m-%d-%y-%H-%M')
-./i2pd --log file --logfile ~/Desktop/i2pd-${now}.log --loglevel debug
+#!/bin/bash
+
+# Check if an argument is provided; if not, default to desktop
+LOG_FILE_PATH="$1"
+if [ -z "$LOG_FILE_PATH" ]; then
+  LOG_FILE_PATH="~/Desktop/i2pd.log"
+fi
+
+# Run the i2pd command with the provided or default log file path
+./i2pd --log file --logfile $LOG_FILE_PATH --loglevel debug
